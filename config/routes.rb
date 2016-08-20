8c41d57c2cdf7581e662fc924a726db080b9d3b6
Daniel <truenorthtrainingcompany@gmail.com>
2016-08-20 15:04:29 -0400

    User model and routes No EDIT or NEW route, this is OK User has token, password and name must be validated

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	# Routes for NEW and EDIT not created because this is an --api app!
+	resources :users
 end
