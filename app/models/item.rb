dd9d4f3590785d63af23c4bd143c95245530be20
Daniel <truenorthtrainingcompany@gmail.com>
2016-08-20 17:04:12 -0400

    /users returns JSON with items relationship ERR: model ass'ns corrected, temporarily commented out Created a test item in console, when visiting users index, returns JSON:API showing user and related items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
 	belongs_to :user
-	has_and_belongs_to_many :categories
-	has_and_belongs_to :status
+	# has_and_belongs_to_many :categories
+	# belongs_to :status
 end
--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,6 +1,6 @@
-class ItemSerializer < ActiveModel::Serilizer
+class ItemSerializer < ActiveModel::Serializer
 	attributes :id, :title, :description, :created_at
 	belongs_to :user
-	has_and_belongs_to_many :categories
-	has_and_belongs_to :status
+	# has_and_belongs_to_many :categories
+	# belongs_to :status
 end
