f410c2487fea40823e8cb6f25f206ee882eee814
Daniel <truenorthtrainingcompany@gmail.com>
2016-08-21 19:33:12 -0400

    Errors corrected, rails t succeeds

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::API
 
 	private
 	def render_error(resource, status)
-		render json: resource, status: status, adapter, :json_api,
+		render json: resource, status: status, adapter: :json_api,
 			serializer: ActiveModel::Serializer::ErrorSerializer
 	end
 
