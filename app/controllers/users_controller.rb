0c272778a19869d43ed49e6733c835d5d68eb397
Daniel <truenorthtrainingcompany@gmail.com>
2016-08-21 16:50:13 -0400

    User Create, user_params Added to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
 class UsersController < ApplicationController
+	before_action :validate_user, only: [:create, :update, :destroy]
+	before_action :validate_type, only: [:create, :update]
+
+	def create
+		user = User.new(user_params)
+		if user.save
+			render json: user, status: :created
+		else
+			render_error(user, :unprocessable_entity)
+		end
+	end
+
 	def index
 		users = User.all
 		render json: users
@@ -17,5 +29,9 @@ class UsersController < ApplicationController
 			user.errors.add(:id, "Wrong ID provided")
 			render_error(user, 404) and return
 		end
+	end
+
+	def user_params
+		ActiveModelSerializers::Deserialization.jsonapi_parse(params)
 	end
 end
