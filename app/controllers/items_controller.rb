ea4484fd7e94b010feafd91a6f85fe1c8689f6b7
Daniel <truenorthtrainingcompany@gmail.com>
2016-08-22 21:27:35 -0400

    Items show route items/id JSON data includes categories

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,26 @@
 class ItemsController < ApplicationController
+	before_action :set_item, only: [:show, :update, :destroy]
 
 	def index
 		items = Item.all
 		render json: items
 	end
+
+	def show
+		render json: @item
+	end
+
+	private
+	def set_item
+		begin
+			@item = Item.find params[:id]
+		rescue ActiveRecord::RecordNotFound
+			item = Item.new
+			item.errors.add(:id, "Wrong ID provided")
+			render_error(user, 404) and return
+		end
+	end
+  def item_params
+    ActiveModelSerializers::Deserialization.jsonapi_parse(params)
+  end
 end
--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,6 +1,7 @@
 class ItemSerializer < ActiveModel::Serializer
 	attributes :id, :title, :description, :created_at
 	belongs_to :user
-	# has_and_belongs_to_many :categories
+  has_many :categories
+	link(:self) { user_url(object) }
 	# belongs_to :status
 end
