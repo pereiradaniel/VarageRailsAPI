93822cc0dce5e24135fd418ffd7ec282fc832569
Daniel <truenorthtrainingcompany@gmail.com>
2016-08-22 21:37:32 -0400

    1 Test added OK Gets valid item data

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -10,4 +10,14 @@ class ItemsControllerTest < ActionController::TestCase
 		assert_equal Item.count, jdata['data'].length
 		assert_equal jdata['data'][0]['type'], 'items'
 	end
+
+  test "Should get valid item data" do
+    item = items('item_1_2')
+    get :show, params: { id: item.id }
+    assert_response :success
+    jdata = JSON.parse response.body
+    assert_equal item.id.to_s, jdata['data']['id']
+    assert_equal item.title, jdata['data']['attributes']['title']
+  end
+
 end
